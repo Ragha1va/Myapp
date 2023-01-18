50205005535bf6f26feeb9741eb887b2510c55c9
Ragha1va <122955092+Ragha1va@users.noreply.github.com>
2023-01-18 10:13:55 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CreateCustomerComponent } from './create-customer/create-customer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerListComponent,
+    CreateCustomerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ customer.ts
@@ -0,0 +1,10 @@
+export class Customer {
+    id: number;
+    firstName: string;
+    lastNane: string;
+    UserName: string;
+    emailId: string;
+    address: string;
+    state: string;
+
+}
